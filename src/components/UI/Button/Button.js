7d429821b7e1b5a1be1a76fbea87d58c929dfef6
olhachuryk-dev <olya1churyk@gmail.com>
2022-09-14 12:31:17 +0300

    Styled component reworked to CSS module

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,33 +1,37 @@
-// import React from 'react';
-// import './Button.css';
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+//Using CSS modules:
 
-import styled from 'styled-components';
+import React from 'react';
+import styles from './Button.module.css';
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
-const Button = styled.button`
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #6c094b;
-  color: white;
-  background: #6c094b;
-  box-shadow: 0 0 4px rgba(234, 234, 234, 0.26);
-  cursor: pointer;
+//Using Styled components:
 
-&:focus {
-  outline: none;
-}
+// import styled from 'styled-components';
 
-&:hover,
-&:active {
-  background: #6c094b;
-  border-color: #6c094b;
-  box-shadow: 0 0 8px rgba(234, 234, 234, 0.26);
-}`
+// const Button = styled.button`
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #6c094b;
+//   color: white;
+//   background: #6c094b;
+//   box-shadow: 0 0 4px rgba(234, 234, 234, 0.26);
+//   cursor: pointer;
+
+// &:focus {
+//   outline: none;
+// }
+
+// &:hover,
+// &:active {
+//   background: #6c094b;
+//   border-color: #6c094b;
+//   box-shadow: 0 0 8px rgba(234, 234, 234, 0.26);
+// }`
 
 export default Button;
